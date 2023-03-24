615b320008a2f66cddfd874d1b15cea2ef1e12cf
y0na24 <y0na24@mail.ru>
2023-03-24 19:27:24 +0300

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,51 @@
+import acceptBtn from './acceptBtn.svg'
+import addNewCategory from './addNewCategory.svg'
+import addNewProject from './addNewProject.svg'
+import calendar from './calendar.svg'
+import cancelBtn from './cancelBtn.svg'
+import dropDownArrow from './dropDownArrow.svg'
+import exit from './exit.svg'
+import gradientBg from './gradientBg.jpg'
+import paginationArrow from './paginationArrow.svg'
+import pauseBtn from './pauseBtn.svg'
+import playBtn from './playBtn.svg'
+import profileAvatar from './profileAvatar.jpg'
+import profileHeader from './profileHeader.jpg'
+import projectSymbol from './projectSymbol.svg'
+import settings from './settings.svg'
+import tempozeLogo from './tempozeLogo.svg'
+import tempozeTitleBlack from './tempozeTitleBlack.svg'
+import tempozeTitleWhite from './tempozeTitleWhite.svg'
+import toolbarAnalytics from './toolbarAnalytics.svg'
+import toolbarCreate from './toolbarCreate.svg'
+import toolbarHome from './toolbarHome.svg'
+import toolbarProfile from './toolbarProfile.svg'
+import filter from './filter.svg'
+import login from './login.svg'
+
+export {
+	acceptBtn,
+	addNewCategory,
+	addNewProject,
+	calendar,
+	cancelBtn,
+	dropDownArrow,
+	exit,
+	gradientBg,
+	paginationArrow,
+	pauseBtn,
+	playBtn,
+	profileAvatar,
+	profileHeader,
+	projectSymbol,
+	settings,
+	tempozeLogo,
+	tempozeTitleBlack,
+	tempozeTitleWhite,
+	toolbarAnalytics,
+	toolbarCreate,
+	toolbarHome,
+	toolbarProfile,
+	filter,
+	login,
+}
