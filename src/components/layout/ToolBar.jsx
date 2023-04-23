8cf4325d4d72d0a1e77b55fd19cac0ce8e80d19d
y0na24 <y0na24@mail.ru>
2023-04-23 19:25:29 +0300

    active class for toolbar item

--- src/components/layout/ToolBar.jsx
+++ src/components/layout/ToolBar.jsx
@@ -2,55 +2,58 @@ import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
 import {
-  toolbarAnalytics,
-  toolbarCreate,
-  toolbarHome,
-  toolbarProfile,
+	toolbarAnalytics,
+	toolbarCreate,
+	toolbarHome,
+	toolbarProfile,
 } from '@/assets/assets'
 
 function ToolBar({ display }) {
-  return (
-    <div className={`${display} pb-3 mx-3`}>
-      <nav className='bg-mainColor py-[1.625rem] px-7 rounded-xl'>
-        <ul className='flex items-center justify-between'>
-          <li>
-            <NavLink to='/'>
-              <img src={toolbarHome} alt='Главная' width='20' height='20' />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='history'>
-              <img
-                src={toolbarCreate}
-                alt='Создать задачу'
-                width='20'
-                height='20'
-              />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/analytics'>
-              <img
-                src={toolbarAnalytics}
-                alt='Аналитика'
-                width='20'
-                height='20'
-              />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/profile'>
-              <img src={toolbarProfile} alt='Профиль' width='20' height='20' />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+	return (
+		<div className={`${display} pb-3 mx-3`}>
+			<nav className='bg-mainColor py-[1.625rem] px-7 rounded-xl'>
+				<ul className='flex items-center justify-between'>
+					<li>
+						<NavLink to='/' className={({ isActive }) => (isActive ? 'toolbar-active' : '')}>
+							<img src={toolbarHome} alt='Главная' width='20' height='20' />
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to='history'
+							className={({ isActive }) => (isActive ? 'toolbar-active' : '')}
+						>
+							<img
+								src={toolbarCreate}
+								alt='Создать задачу'
+								width='20'
+								height='20'
+							/>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/analytics' className={({ isActive }) => (isActive ? 'toolbar-active' : '')}>
+							<img
+								src={toolbarAnalytics}
+								alt='Аналитика'
+								width='20'
+								height='20'
+							/>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/profile' className={({ isActive }) => (isActive ? 'toolbar-active' : '')}>
+							<img src={toolbarProfile} alt='Профиль' width='20' height='20' />
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	)
 }
 
 ToolBar.propTypes = {
-  display: PropTypes.string.isRequired,
+	display: PropTypes.string.isRequired,
 }
 
 export default ToolBar
