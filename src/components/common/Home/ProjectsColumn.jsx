fa048357f0b45e13d77a030ca056378c55c379b9
y0na24 <y0na24@mail.ru>
2023-04-23 19:59:35 +0300

    forgot to lint :)

--- src/components/common/Home/ProjectsColumn.jsx
+++ src/components/common/Home/ProjectsColumn.jsx
@@ -8,31 +8,30 @@ import AddButton from '@/components/ui/Buttons/AddButton'
 import { addNewProject } from '@/assets/assets'
 
 function ProjectsColumn({ onClick, display }) {
-	const currentUrl = useLocation()
+  const currentUrl = useLocation()
 
-	const displayStyles =
-		display === 'mobile' ? 'flex sm:hidden' : 'hidden sm:flex'
+  const displayStyles =		display === 'mobile' ? 'flex sm:hidden' : 'hidden sm:flex'
 
-	return (
-		<div
-			className={`${displayStyles} flex-col p-6 rounded-xl sm:bg-mainColor mt-8 sm:mt-0`}
-		>
-			<div className='flex-1'>
-				<ProjectList amount={1} title='Личные проекты' />
-				<ProjectList amount={3} title='Командные проекты' />
-			</div>
-			{currentUrl.pathname.includes('history') ? (
-				<AddButton onClick={onClick} margin='ml-auto' image={addNewProject} />
-			) : (
-				<CreateButton onClick={onClick} />
-			)}
-		</div>
-	)
+  return (
+    <div
+      className={`${displayStyles} flex-col p-6 rounded-xl sm:bg-mainColor mt-8 sm:mt-0`}
+    >
+      <div className='flex-1'>
+        <ProjectList amount={1} title='Личные проекты' />
+        <ProjectList amount={3} title='Командные проекты' />
+      </div>
+      {currentUrl.pathname.includes('history') ? (
+        <AddButton onClick={onClick} margin='ml-auto' image={addNewProject} />
+      ) : (
+        <CreateButton onClick={onClick} />
+      )}
+    </div>
+  )
 }
 
 ProjectsColumn.propTypes = {
-	onClick: PropTypes.func.isRequired,
-	display: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  display: PropTypes.string.isRequired,
 }
 
 export default ProjectsColumn
--- src/components/page/HistoryPage/HistoryPage.jsx
+++ src/components/page/HistoryPage/HistoryPage.jsx
@@ -4,17 +4,17 @@ import ProjectsColumn from '@/components/common/Home/ProjectsColumn'
 import AddProjectColumn from '@/components/common/Home/AddProjectColumn'
 
 function HistoryPage() {
-	const [isAdd, setIsAdd] = React.useState(false)
+  const [isAdd, setIsAdd] = React.useState(false)
 
-	const handleClick = () => {
-		setIsAdd(!isAdd)
-	}
+  const handleClick = () => {
+    setIsAdd(!isAdd)
+  }
 
-	return isAdd ? (
-		<AddProjectColumn onClick={handleClick} />
-	) : (
-		<ProjectsColumn display='mobile' onClick={handleClick} />
-	)
+  return isAdd ? (
+    <AddProjectColumn onClick={handleClick} />
+  ) : (
+    <ProjectsColumn display='mobile' onClick={handleClick} />
+  )
 }
 
 export default HistoryPage
