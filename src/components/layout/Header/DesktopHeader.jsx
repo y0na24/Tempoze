152cea0e4d4ec48250eee7c5be81722b5046736e
y0na24 <y0na24@mail.ru>
2023-05-27 18:29:10 +0300

    userProfile with drop down added

--- src/components/layout/Header/DesktopHeader.jsx
+++ src/components/layout/Header/DesktopHeader.jsx
@@ -1,5 +1,7 @@
 import { NavLink, useLocation } from 'react-router-dom'
 
+import UserProfile from '@/components/common/UserProfile'
+
 function DesktopHeader() {
   const location = useLocation()
 
@@ -10,20 +12,23 @@ function DesktopHeader() {
     : null
 
   return (
-    <nav className='bg-black w-full m-auto px-[2.1875rem] py-[1.125rem] rounded-xl'>
-      <ul className='text-white font-semibold text-base flex gap-8'>
-        <NavLink to='/home' className={isDisabledLink}>
-          <li className='hover:opacity-70 transition duration-200 uppercase'>
-            Главная
-          </li>
-        </NavLink>
-        <NavLink to='/analytics' className={isDisabledLink}>
-          <li className='hover:opacity-70 transition duration-200 uppercase'>
-            Аналитика
-          </li>
-        </NavLink>
-      </ul>
-    </nav>
+    <div>
+      <nav className='bg-black flex items-center justify-between w-full m-auto px-[2.1875rem] py-[1.125rem] rounded-xl'>
+        <ul className='text-white font-semibold text-base flex gap-8'>
+          <NavLink to='/home' className={isDisabledLink}>
+            <li className='hover:opacity-70 transition duration-200 uppercase'>
+              Главная
+            </li>
+          </NavLink>
+          <NavLink to='/analytics' className={isDisabledLink}>
+            <li className='hover:opacity-70 transition duration-200 uppercase'>
+              Аналитика
+            </li>
+          </NavLink>
+        </ul>
+        {!isStartingPage && <UserProfile userName='userName' />}
+      </nav>
+    </div>
   )
 }
 
---
+++ src/components/ui/DropDown.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import { settings, exit } from '@/assets/assets'
+
+function DropDown({ onClick }) {
+  return (
+    <div className='bg-black absolute rounded-xl right-[-35px] top-[65px] inline-block py-6 px-[2.375rem]'>
+      <ul className='flex flex-col gap-4  '>
+        <Link to='/profile' className='flex gap-3' onClick={onClick}>
+          <p className='uppercase text-base font-medium'>Настройки</p>
+          <img src={settings} alt='Настройки' />
+        </Link>
+        <Link to='/' className='flex gap-3 self-end' onClick={onClick}>
+          <p className='uppercase text-base font-medium'>Выйти</p>
+          <img src={exit} alt='Выход' />
+        </Link>
+      </ul>
+    </div>
+  )
+}
+
+DropDown.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+
+export default DropDown
