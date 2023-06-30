5f6d96921047dc88451a66f5d753692d9c3e9e4e
y0na24 <y0na24@mail.ru>
2023-06-30 13:54:29 +0300

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tempoze/',
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-  },
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+		],
+	},
 })
