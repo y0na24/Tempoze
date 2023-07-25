fd48d8135fde557a6d18660228e1a841a741d43d
y0na24 <y0na24@mail.ru>
2023-07-25 14:10:20 +0300

    docker

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const path = require('path')
 const chalk = require('chalk')
-const cors = require('cors');
-// const initDatabase = require('./startUp/iniitDatabase')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
@@ -14,19 +14,20 @@ app.use(cors())
 
 app.use('/api', routes)
 
-const PORT = config.get('port') ?? 5050
+const PORT = config.get('port') ?? 8080
 
-// if (process.env.NODE_ENV === 'production') {
-//   console.log('Production')
-// } else {
-//   console.log('Development')
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')))
+
+  const indexPath = path.join(__dirname, 'client', 'index.html')
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath)
+  })
+}
 
 async function start() {
   try {
-    // mongoose.connection.once('open', () => {
-    //   initDatabase()
-    // })
     await mongoose.connect(config.get('mongoUri'))
     app.listen(PORT, () => console.log(chalk.green(`Server has been started on port: ${PORT}...`)))
   } catch (e) {
